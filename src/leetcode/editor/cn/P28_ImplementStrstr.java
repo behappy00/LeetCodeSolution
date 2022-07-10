d4d4b877e52f74c15689ec43d2566945049a90de
behappy00 <172310978@qq.com>
2022-07-10 10:30:15 +0800

    2022-07-10

--- src/leetcode/editor/cn/P28_ImplementStrstr.java
+++ src/leetcode/editor/cn/P28_ImplementStrstr.java
@@ -38,6 +38,7 @@ package leetcode.editor.cn;
 
 /**
  * Implement strStr()
+ *
  * @author WBJ
  * @date 2022-07-09 23:57:56
  */
@@ -49,11 +50,60 @@ public class P28_ImplementStrstr {
 
     //力扣代码
 //leetcode submit region begin(Prohibit modification and deletion)
+
+//    KMP算法
     class Solution {
         public int strStr(String haystack, String needle) {
-
+            int n = haystack.length(), m = needle.length();
+            if (m == 0) {
+                return 0;
+            }
+            int[] pi = new int[m];
+            for (int i = 1, j = 0; i < m; i++) {
+                while (j > 0 && needle.charAt(i) != needle.charAt(j)) {
+                    j = pi[j - 1];
+                }
+                if (needle.charAt(i) == needle.charAt(j)) {
+                    j++;
+                }
+                pi[i] = j;
+            }
+            for (int i = 0, j = 0; i < n; i++) {
+                while (j > 0 && haystack.charAt(i) != needle.charAt(j)) {
+                    j = pi[j - 1];
+                }
+                if (haystack.charAt(i) == needle.charAt(j)) {
+                    j++;
+                }
+                if (j == m) {
+                    return i - m + 1;
+                }
+            }
+            return -1;
         }
     }
+
+//    暴力破解
+//    class Solution {
+//        public int strStr(String haystack, String needle) {
+//
+//            int n = haystack.length(), m = needle.length();
+//            for (int i = 0; i + m <= n; i++) {
+//                boolean flag = true;
+//                for (int j = 0; j < m; j++) {
+//                    if (haystack.charAt(i + j) != needle.charAt(j)) {
+//                        flag = false;
+//                        break;
+//                    }
+//                }
+//                if (flag) {
+//                    return i;
+//                }
+//            }
+//            return -1;
+//        }
+//    }
+
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
