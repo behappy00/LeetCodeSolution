cf71100eff7cfa9e9a5903018649da0fdcb194ce
behappy00 <172310978@qq.com>
2022-07-10 10:24:07 +0800

    2022-07-10

--- src/leetcode/editor/cn/P27_RemoveElement.java
+++ src/leetcode/editor/cn/P27_RemoveElement.java
@@ -76,7 +76,15 @@ public class P27_RemoveElement {
 //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int removeElement(int[] nums, int val) {
-
+            int n = nums.length ;
+            int j = 0;
+            for(int i =0; i<n;i++){
+                if(nums[i] != val){
+                    nums[j] =nums[i];
+                    j++;
+                }
+            }
+            return j;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
