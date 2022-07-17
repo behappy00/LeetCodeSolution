d533bb6731a63833ae09f20741776622b85ce94d
behappy00 <172310978@qq.com>
2022-07-17 15:47:55 +0800

    2022-07-17

---
+++ src/leetcode/editor/cn/P100_SameTree.java
@@ -0,0 +1,77 @@
+/**
+<p>Given the roots of two binary trees <code>p</code> and <code>q</code>, write a function to check if they are the same or not.</p>
+
+<p>Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.</p>
+
+<p>&nbsp;</p>
+<p><strong>Example 1:</strong></p>
+<img alt="" src="https://assets.leetcode.com/uploads/2020/12/20/ex1.jpg" style="width: 622px; height: 182px;" />
+<pre>
+<strong>Input:</strong> p = [1,2,3], q = [1,2,3]
+<strong>Output:</strong> true
+</pre>
+
+<p><strong>Example 2:</strong></p>
+<img alt="" src="https://assets.leetcode.com/uploads/2020/12/20/ex2.jpg" style="width: 382px; height: 182px;" />
+<pre>
+<strong>Input:</strong> p = [1,2], q = [1,null,2]
+<strong>Output:</strong> false
+</pre>
+
+<p><strong>Example 3:</strong></p>
+<img alt="" src="https://assets.leetcode.com/uploads/2020/12/20/ex3.jpg" style="width: 622px; height: 182px;" />
+<pre>
+<strong>Input:</strong> p = [1,2,1], q = [1,1,2]
+<strong>Output:</strong> false
+</pre>
+
+<p>&nbsp;</p>
+<p><strong>Constraints:</strong></p>
+
+<ul>
+	<li>The number of nodes in both trees is in the range <code>[0, 100]</code>.</li>
+	<li><code>-10<sup>4</sup> &lt;= Node.val &lt;= 10<sup>4</sup></code></li>
+</ul>
+<div><div>Related Topics</div><div><li>树</li><li>深度优先搜索</li><li>广度优先搜索</li><li>二叉树</li></div></div><br><div><li>👍 862</li><li>👎 0</li></div>
+*/
+
+package leetcode.editor.cn;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * Same Tree
+ * @author WBJ
+ * @date 2022-07-17 15:04:49
+ */
+public class P100_SameTree{
+	 public static void main(String[] args) {
+	 	 //测试代码
+	 	 Solution solution = new P100_SameTree().new Solution();
+	 }
+	 
+//力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/leetcode/editor/cn/P83_RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,62 @@
+/**
+<p>Given the <code>head</code> of a sorted linked list, <em>delete all duplicates such that each element appears only once</em>. Return <em>the linked list <strong>sorted</strong> as well</em>.</p>
+
+<p>&nbsp;</p>
+<p><strong>Example 1:</strong></p>
+<img alt="" src="https://assets.leetcode.com/uploads/2021/01/04/list1.jpg" style="width: 302px; height: 242px;" />
+<pre>
+<strong>Input:</strong> head = [1,1,2]
+<strong>Output:</strong> [1,2]
+</pre>
+
+<p><strong>Example 2:</strong></p>
+<img alt="" src="https://assets.leetcode.com/uploads/2021/01/04/list2.jpg" style="width: 542px; height: 222px;" />
+<pre>
+<strong>Input:</strong> head = [1,1,2,3,3]
+<strong>Output:</strong> [1,2,3]
+</pre>
+
+<p>&nbsp;</p>
+<p><strong>Constraints:</strong></p>
+
+<ul>
+	<li>The number of nodes in the list is in the range <code>[0, 300]</code>.</li>
+	<li><code>-100 &lt;= Node.val &lt;= 100</code></li>
+	<li>The list is guaranteed to be <strong>sorted</strong> in ascending order.</li>
+</ul>
+<div><div>Related Topics</div><div><li>链表</li></div></div><br><div><li>👍 823</li><li>👎 0</li></div>
+*/
+
+package leetcode.editor.cn;
+
+/**
+ * Remove Duplicates from Sorted List
+ * @author WBJ
+ * @date 2022-07-17 15:04:42
+ */
+public class P83_RemoveDuplicatesFromSortedList{
+	 public static void main(String[] args) {
+	 	 //测试代码
+	 	 Solution solution = new P83_RemoveDuplicatesFromSortedList().new Solution();
+	 }
+	 
+//力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
