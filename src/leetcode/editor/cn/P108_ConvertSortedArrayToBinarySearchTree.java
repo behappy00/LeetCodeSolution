6598fcfa924288f605ad68b194e8f248d2aa69dd
behappy00 <172310978@qq.com>
2022-07-18 20:32:14 +0800

    2022-07-18

--- src/leetcode/editor/cn/P108_ConvertSortedArrayToBinarySearchTree.java
+++ src/leetcode/editor/cn/P108_ConvertSortedArrayToBinarySearchTree.java
@@ -34,8 +34,11 @@
 
 package leetcode.editor.cn;
 
+import utils.TreeNode;
+
 /**
  * Convert Sorted Array to Binary Search Tree
+ *
  * @author WBJ
  * @date 2022-07-17 21:09:04
  */
@@ -65,8 +68,22 @@ public class P108_ConvertSortedArrayToBinarySearchTree {
 	 */
 	class Solution {
 		public TreeNode sortedArrayToBST(int[] nums) {
-
+			return dfs(nums, 0, nums.length - 1);
 		}
+
+		private TreeNode dfs(int[] nums, int lo, int hi) {
+			if (lo > hi) {
+				return null;
+			}
+			// 以升序数组的中间元素作为根节点 root。
+			int mid = lo + (hi - lo) / 2;
+			TreeNode root = new TreeNode(nums[mid]);
+			// 递归的构建 root 的左子树与右子树。
+			root.left = dfs(nums, lo, mid - 1);
+			root.right = dfs(nums, mid + 1, hi);
+			return root;
+		}
+
 	}
 //leetcode submit region end(Prohibit modification and deletion)
 
