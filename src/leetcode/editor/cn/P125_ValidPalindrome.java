2ecba0dfa0f263442d1beefd5d732ec83ec46134
behappy00 <172310978@qq.com>
2022-07-24 21:32:23 +0800

    2022-07-24

---
+++ src/leetcode/editor/cn/P125_ValidPalindrome.java
@@ -0,0 +1,64 @@
+/**
+ * <p>A phrase is a <strong>palindrome</strong> if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.</p>
+ *
+ * <p>Given a string <code>s</code>, return <code>true</code><em> if it is a <strong>palindrome</strong>, or </em><code>false</code><em> otherwise</em>.</p>
+ *
+ * <p>&nbsp;</p>
+ * <p><strong>Example 1:</strong></p>
+ *
+ * <pre>
+ * <strong>Input:</strong> s = &quot;A man, a plan, a canal: Panama&quot;
+ * <strong>Output:</strong> true
+ * <strong>Explanation:</strong> &quot;amanaplanacanalpanama&quot; is a palindrome.
+ * </pre>
+ *
+ * <p><strong>Example 2:</strong></p>
+ *
+ * <pre>
+ * <strong>Input:</strong> s = &quot;race a car&quot;
+ * <strong>Output:</strong> false
+ * <strong>Explanation:</strong> &quot;raceacar&quot; is not a palindrome.
+ * </pre>
+ *
+ * <p><strong>Example 3:</strong></p>
+ *
+ * <pre>
+ * <strong>Input:</strong> s = &quot; &quot;
+ * <strong>Output:</strong> true
+ * <strong>Explanation:</strong> s is an empty string &quot;&quot; after removing non-alphanumeric characters.
+ * Since an empty string reads the same forward and backward, it is a palindrome.
+ * </pre>
+ *
+ * <p>&nbsp;</p>
+ * <p><strong>Constraints:</strong></p>
+ *
+ * <ul>
+ * <li><code>1 &lt;= s.length &lt;= 2 * 10<sup>5</sup></code></li>
+ * <li><code>s</code> consists only of printable ASCII characters.</li>
+ * </ul>
+ * <div><div>Related Topics</div><div><li>双指针</li><li>字符串</li></div></div><br><div><li>👍 553</li><li>👎 0</li></div>
+ */
+
+package leetcode.editor.cn;
+
+/**
+ * Valid Palindrome
+ * @author WBJ
+ * @date 2022-07-24 21:19:23
+ */
+public class P125_ValidPalindrome {
+	public static void main(String[] args) {
+		//测试代码
+		Solution solution = new P125_ValidPalindrome().new Solution();
+	}
+
+	//力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+	class Solution {
+		public boolean isPalindrome(String s) {
+
+		}
+	}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
