f2dd021483db9e6e43cb66aae069053b6a188f6d
behappy00 <172310978@qq.com>
2022-07-25 22:50:49 +0800

    2022-07-25

--- src/leetcode/editor/cn/P136_SingleNumber.java
+++ src/leetcode/editor/cn/P136_SingleNumber.java
@@ -1,28 +1,28 @@
-/**
- * <p>Given a <strong>non-empty</strong>&nbsp;array of integers <code>nums</code>, every element appears <em>twice</em> except for one. Find that single one.</p>
- *
- * <p>You must&nbsp;implement a solution with a linear runtime complexity and use&nbsp;only constant&nbsp;extra space.</p>
- *
- * <p>&nbsp;</p>
- * <p><strong>Example 1:</strong></p>
- * <pre><strong>Input:</strong> nums = [2,2,1]
- * <strong>Output:</strong> 1
- * </pre><p><strong>Example 2:</strong></p>
- * <pre><strong>Input:</strong> nums = [4,1,2,1,2]
- * <strong>Output:</strong> 4
- * </pre><p><strong>Example 3:</strong></p>
- * <pre><strong>Input:</strong> nums = [1]
- * <strong>Output:</strong> 1
- * </pre>
- * <p>&nbsp;</p>
- * <p><strong>Constraints:</strong></p>
- *
- * <ul>
- * <li><code>1 &lt;= nums.length &lt;= 3 * 10<sup>4</sup></code></li>
- * <li><code>-3 * 10<sup>4</sup> &lt;= nums[i] &lt;= 3 * 10<sup>4</sup></code></li>
- * <li>Each element in the array appears twice except for one element which appears only once.</li>
- * </ul>
- * <div><div>Related Topics</div><div><li>位运算</li><li>数组</li></div></div><br><div><li>👍 2507</li><li>👎 0</li></div>
+/*
+  <p>Given a <strong>non-empty</strong>&nbsp;array of integers <code>nums</code>, every element appears <em>twice</em> except for one. Find that single one.</p>
+
+  <p>You must&nbsp;implement a solution with a linear runtime complexity and use&nbsp;only constant&nbsp;extra space.</p>
+
+  <p>&nbsp;</p>
+  <p><strong>Example 1:</strong></p>
+  <pre><strong>Input:</strong> nums = [2,2,1]
+  <strong>Output:</strong> 1
+  </pre><p><strong>Example 2:</strong></p>
+  <pre><strong>Input:</strong> nums = [4,1,2,1,2]
+  <strong>Output:</strong> 4
+  </pre><p><strong>Example 3:</strong></p>
+  <pre><strong>Input:</strong> nums = [1]
+  <strong>Output:</strong> 1
+  </pre>
+  <p>&nbsp;</p>
+  <p><strong>Constraints:</strong></p>
+
+  <ul>
+  <li><code>1 &lt;= nums.length &lt;= 3 * 10<sup>4</sup></code></li>
+  <li><code>-3 * 10<sup>4</sup> &lt;= nums[i] &lt;= 3 * 10<sup>4</sup></code></li>
+  <li>Each element in the array appears twice except for one element which appears only once.</li>
+  </ul>
+  <div><div>Related Topics</div><div><li>位运算</li><li>数组</li></div></div><br><div><li>👍 2507</li><li>👎 0</li></div>
  */
 
 package leetcode.editor.cn;
@@ -43,6 +43,19 @@ public class P136_SingleNumber {
 //leetcode submit region begin(Prohibit modification and deletion)
 	class Solution {
 		public int singleNumber(int[] nums) {
+//			int ans = nums[0];
+//			if (nums.length > 1) {
+//				for (int i = 1; i < nums.length; i++) {
+//					ans = ans ^ nums[i];
+//				}
+//			}
+//			return ans;
+
+			int single = 0;
+			for (int num : nums) {
+				single ^= num;
+			}
+			return single;
 
 		}
 	}
