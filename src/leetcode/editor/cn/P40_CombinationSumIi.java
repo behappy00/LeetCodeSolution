f03250b7d2d458075600b7927f377044aa506450
behappy00 <172310978@qq.com>
2022-08-04 10:43:09 +0800

    2022-08-04

---
+++ src/leetcode/editor/cn/P40_CombinationSumIi.java
@@ -0,0 +1,68 @@
+package leetcode.editor.cn;
+
+/**
+ * <p>Given a collection of candidate numbers (<code>candidates</code>) and a target number (<code>target</code>), find all unique combinations in <code>candidates</code>&nbsp;where the candidate numbers sum to <code>target</code>.</p>
+ *
+ * <p>Each number in <code>candidates</code>&nbsp;may only be used <strong>once</strong> in the combination.</p>
+ *
+ * <p><strong>Note:</strong>&nbsp;The solution set must not contain duplicate combinations.</p>
+ *
+ * <p>&nbsp;</p>
+ * <p><strong>Example 1:</strong></p>
+ *
+ * <pre>
+ * <strong>Input:</strong> candidates = [10,1,2,7,6,1,5], target = 8
+ * <strong>Output:</strong>
+ * [
+ * [1,1,6],
+ * [1,2,5],
+ * [1,7],
+ * [2,6]
+ * ]
+ * </pre>
+ *
+ * <p><strong>Example 2:</strong></p>
+ *
+ * <pre>
+ * <strong>Input:</strong> candidates = [2,5,2,1,2], target = 5
+ * <strong>Output:</strong>
+ * [
+ * [1,2,2],
+ * [5]
+ * ]
+ * </pre>
+ *
+ * <p>&nbsp;</p>
+ * <p><strong>Constraints:</strong></p>
+ *
+ * <ul>
+ * <li><code>1 &lt;=&nbsp;candidates.length &lt;= 100</code></li>
+ * <li><code>1 &lt;=&nbsp;candidates[i] &lt;= 50</code></li>
+ * <li><code>1 &lt;= target &lt;= 30</code></li>
+ * </ul>
+ * <div><div>Related Topics</div><div><li>数组</li><li>回溯</li></div></div><br><div><li>👍 1054</li><li>👎 0</li></div>
+ */
+
+import java.util.List;
+
+/**
+ * Combination Sum II
+ * @author WBJ
+ * @date 2022-08-04 10:34:33
+ */
+public class P40_CombinationSumIi {
+	public static void main(String[] args) {
+		//测试代码
+		Solution solution = new P40_CombinationSumIi().new Solution();
+	}
+
+	//力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+	class Solution {
+		public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+
+		}
+	}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/leetcode/editor/cn/P49_GroupAnagrams.java
@@ -0,0 +1,52 @@
+package leetcode.editor.cn;
+
+/**
+ * <p>Given an array of strings <code>strs</code>, group <strong>the anagrams</strong> together. You can return the answer in <strong>any order</strong>.</p>
+ *
+ * <p>An <strong>Anagram</strong> is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.</p>
+ *
+ * <p>&nbsp;</p>
+ * <p><strong>Example 1:</strong></p>
+ * <pre><strong>Input:</strong> strs = ["eat","tea","tan","ate","nat","bat"]
+ * <strong>Output:</strong> [["bat"],["nat","tan"],["ate","eat","tea"]]
+ * </pre><p><strong>Example 2:</strong></p>
+ * <pre><strong>Input:</strong> strs = [""]
+ * <strong>Output:</strong> [[""]]
+ * </pre><p><strong>Example 3:</strong></p>
+ * <pre><strong>Input:</strong> strs = ["a"]
+ * <strong>Output:</strong> [["a"]]
+ * </pre>
+ * <p>&nbsp;</p>
+ * <p><strong>Constraints:</strong></p>
+ *
+ * <ul>
+ * <li><code>1 &lt;= strs.length &lt;= 10<sup>4</sup></code></li>
+ * <li><code>0 &lt;= strs[i].length &lt;= 100</code></li>
+ * <li><code>strs[i]</code> consists of lowercase English letters.</li>
+ * </ul>
+ * <div><div>Related Topics</div><div><li>数组</li><li>哈希表</li><li>字符串</li><li>排序</li></div></div><br><div><li>👍 1212</li><li>👎 0</li></div>
+ */
+
+import java.util.List;
+
+/**
+ * Group Anagrams
+ * @author WBJ
+ * @date 2022-08-04 10:37:12
+ */
+public class P49_GroupAnagrams {
+	public static void main(String[] args) {
+		//测试代码
+		Solution solution = new P49_GroupAnagrams().new Solution();
+	}
+
+	//力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+	class Solution {
+		public List<List<String>> groupAnagrams(String[] strs) {
+
+		}
+	}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
